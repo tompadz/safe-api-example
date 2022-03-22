989f5c726b769478eab03b2757a9e5e538421a06
Igor' Igor <77836896+tompadz@users.noreply.github.com>
2022-03-22 13:26:35 +0300

    Create ErrorTypeHTTP.kt

---
+++ ErrorTypeHTTP.kt
@@ -0,0 +1,8 @@
+enum class ErrorTypeHTTP(val code:Int, @StringRes val title:Int, @StringRes val desc:Int? = null) {
+    INTERNAL_ERROR(code = 500, title = "error title"),
+    NOT_IMPL_ERROR(code = 501, title = "error title"),
+    BAD_GATEWAY_ERROR(code = 502, title = "error title"),
+    UNAVAILABLE_ERROR(code = 503, title = "error title"),
+    TIMEOUT_ERROR(code = 504, title = "error title"),
+    UNKNOWN_ERROR(code = 1, title = "error title", desc = "error descr"),
+}
